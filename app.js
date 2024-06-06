4231d7d6c2652851b203f82bc59302d73b474d5c
yahia <medyahiak@gmail.com>
2024-06-06 11:36:22 +0100

    home page - adding the intro section

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-document.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.header');
-    const sticky = navbar.offsetTop;
-
-    if (window.pageYOffset >= sticky) {
-        navbar.style.position = 'fixed';
-        navbar.style.top = '0';
-    } else {
-        navbar.style.position = 'absolute';
-        navbar.style.top = '0';
-    }
+document.querySelector('.learnMore').addEventListener('click', function() {
+    document.querySelector('.intro').scrollIntoView({ behavior: 'smooth' });
 });
